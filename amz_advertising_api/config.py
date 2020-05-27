e364bb496d6e6e8347025e31102029a9565a6d7b
854350999 <53171984+854350999@users.noreply.github.com>
2020-05-27 13:44:26 +0800

    Update config.py

--- amz_advertising_api/config.py
+++ amz_advertising_api/config.py
@@ -1,6 +1,6 @@
-client_id = 'amzn1.application-oa2-client.19dd95de9c8148d39528baffb99d403b'
-client_secret = 'db1beaad7a0fe27f693e859f21f023b3e7d5358f18befd9429c818170f812ceb'
-redirect_uri = 'https://www.jsaux.com/'
+client_id = 'xxxxxxxxxxxxx'
+client_secret = 'xxxxxxxx'
+redirect_uri = ''
 domain = {
     'NA': 'https://advertising-api.amazon.com',
     'EU': 'https://advertising-api-eu.amazon.com',
@@ -11,7 +11,6 @@ token_url_list = {
     'EU': 'https://api.amazon.co.uk/auth/o2/token',
     'FE': 'https://api.amazon.co.jp/auth/o2/token'
 }
-refresh_token = 'XXXXXXXXXXXXXXX'
 host_url_list = {
     'NA': 'https://www.amazon.com/ap/oa',
     'EU': 'https://eu.account.amazon.com/ap/oa',
