171c0dc1faa80b2dc674b910c4666fc0c4b899cd
854350999@qq.com <854350999@qq.com>
2022-02-17 15:11:26 +0800

    品牌广告广告组添加filter

--- ad_api/sp_brands/adgroups.py
+++ ad_api/sp_brands/adgroups.py
@@ -5,10 +5,18 @@ from ..client import Client
 
 class AdGroups(Client):
 
-    def get_ad_groups(self, params):
+    def get_ad_groups(self, start_index: int = 0, count: int = None, name: str = None, ad_group_id_filter: str = None,
+                      campaign_id_filter: str = None, creative_type: str = None):
         self.uri_path = "/sb/adGroups"
         self.method = "get"
-        self.data = params
+        self.data = {
+            "startIndex": start_index,
+            "count": count,
+            "name": name,
+            "adGroupIdFilter": ad_group_id_filter,
+            "campaignIdFilter": campaign_id_filter,
+            "creativeType": creative_type
+        }
         return self.execute()
 
     def get_ad_group_by_id(self, ad_group_id):
