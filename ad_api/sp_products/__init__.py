98062a66ecce69af7c568e5f6954a4d2b782d3d4
854350999@qq.com <854350999@qq.com>
2022-11-15 15:40:54 +0800

    取消reporting

--- ad_api/sp_products/__init__.py
+++ ad_api/sp_products/__init__.py
@@ -9,6 +9,6 @@ from .negative_keywords import NegativeKeywords
 from .negative_product_targeting import NegativeProductTargeting
 from .product_ads import ProductAds, ProductAdsV3
 from .product_targeting import ProductTargeting, ProductTargetingV3
-from .reports import Reports, Reporting
+from .reports import Reports
 from .snapshots import Snapshots
 from .suggested_keywords import SuggestedKeywords
