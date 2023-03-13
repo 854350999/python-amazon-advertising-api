5df6dca677531929571ffedd853265969b0dad91
854350999@qq.com <854350999@qq.com>
2023-03-13 17:31:03 +0800

    添加readme的支持

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-amazon-advertising-api",
-    version="0.8.1",
+    version="0.8.2",
     install_requires=[
         "requests"
     ],
